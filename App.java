189d1810efa6b2284a7ed23108b9abbb51c75b57
Sindhu S <sindhu.s1030@gmail.com>
2024-10-18 10:22:39 +0530

    programs

--- App.java
+++ App.java
@@ -1,16 +1,36 @@
-package org.anudip.javaapp;
+/*
+*creating a package
+*/package org.anudip.javaapp;
 
-import org.anudip.javaapp.mathematics.Division;
+/* 
+*importing package and class
+*/import org.anudip.javaapp.operation.TwoNonDecimalNumbersSum;
 
+/*
+*creating main class
+*/
 public class App {
     public static void main(String[] args) {
-		System.out.println("main() started.");
-		
-		Division division = new Division();
-		//division.divide(10, 0);
-		int[] values = {10, 0, 20, 0};
-		division.divide(values);
-		
-		System.out.println("main() ended.");
+	  
+	   /*
+       *creating object
+       */	 
+	   TwoNonDecimalNumbersSum twoNonDecimalNumbersSum = new TwoNonDecimalNumbersSum();
+	   
+	   
+	   long result1 = twoNonDecimalNumbersSum.add(7, 5);
+	   System.out.println(result1);
+	   long result2 = twoNonDecimalNumbersSum.add(-7, 5);
+	   System.out.println(result2);
+	   long result3 = twoNonDecimalNumbersSum.add(7, -5);
+	   System.out.println(result3);
+	   long result4 = twoNonDecimalNumbersSum.add(-7, -5);
+	   System.out.println(result4);
+	   long result5 = twoNonDecimalNumbersSum.add(7, 0);
+	   System.out.println(result5);
+	   long result6 = twoNonDecimalNumbersSum.add(0, 5);
+	   System.out.println(result6);
+	   long result7 = twoNonDecimalNumbersSum.add(0, 0);
+	   System.out.println(result7);
     }
 }
