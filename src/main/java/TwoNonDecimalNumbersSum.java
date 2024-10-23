c020b7d81941e7be4ce92ac27f34e08cad54d875
Ameeda M <ameeda071@gmail.com>
2024-10-23 19:31:50 +0530

    Addition of two non decimal numbers

--- src/main/java/TwoNonDecimalNumbersSum.java
+++ src/main/java/TwoNonDecimalNumbersSum.java
@@ -11,16 +11,21 @@ public class TwoNonDecimalNumbersSum {
 
 /*
 *creating a method with same class name
-*accessible to every class and methods
+*accessible to any class and this method contains
+*two parameters of int type and returns long type 
 */
 
     public long add(int num1, int num2) {
 		int result = 0;
-		//checking if number is negative and return -1
+		/*checking if number is negative and 
+		*if negative return -1
+		*/
 		if (num1 < 0 || num2 < 0) {
 			return -1;
 		} 
-		//checking if number is 0 and return -2
+		/*checking if number is 0 and 
+		*if 0 return -2
+		*/
 		else if(num1 == 0 || num2 == 0) {
 			return -2;
 		} 
