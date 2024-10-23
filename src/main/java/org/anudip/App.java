39f0624a2bb8a21809fb7b7bf91558ec5f7ba52f
Jnanesh2 <jnaneshgowda.2325@gmail.com>
2024-10-23 12:55:37 +0530

    Added java-app-01Solution

--- src/main/java/org/anudip/App.java
+++ src/main/java/org/anudip/App.java
@@ -1,7 +1,35 @@
 package org.anudip;
 
-public class App {
-	public static void main(String[] args) {
-		
-	}
+import org.anudip.javaapp01.operation.TwoNonDecimalNumbersSum;
+
+public class App
+{
+public static void main(String[] args)
+{
+TwoNonDecimalNumbersSum twonondecimalnumberssum = new TwoNonDecimalNumbersSum();
+
+long result1 = twonondecimalnumberssum.add(10,2);
+System.out.println(result1);
+
+long result2 = twonondecimalnumberssum.add(2,10);
+System.out.println(result2);
+
+long result3 = twonondecimalnumberssum.add(10,-2);
+System.out.println(result3);
+
+long result4 = twonondecimalnumberssum.add(-10,2);
+System.out.println(result4);
+
+long result5 = twonondecimalnumberssum.add(-10,-2);
+System.out.println(result5);
+
+long result6 = twonondecimalnumberssum.add(10,0);
+System.out.println(result6);
+
+long result7 = twonondecimalnumberssum.add(0,2);
+System.out.println(result7);
+
+long result8 = twonondecimalnumberssum.add(0,0);
+System.out.println(result8);
+}
 }
